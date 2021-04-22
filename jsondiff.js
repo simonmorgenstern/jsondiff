5c76cd5d79adf8fa00514865c8e0156fd1b63846
Simon Morgenstern <simon.morgenstern@diconium.com>
2021-04-22 13:41:04 +0200

    recursively print out all keys

--- jsondiff.js
+++ jsondiff.js
@@ -1,3 +1,15 @@
 const object1 = require('./obj1.json');
 const object2 = require('./obj2.json');
 
+function printAllKeys(object) {
+    for (key in object) {
+        if (typeof object[key] === "object") {
+            printAllKeys(object[key]);
+        } else {
+            console.log(key);
+        }
+    }
+}
+
+
+printAllKeys(object1);
