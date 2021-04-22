60a954d6ce6796cf7129bb6b6dfd4308a54044b4
Simon Morgenstern <simon.morgenstern@diconium.com>
2021-04-22 15:42:28 +0200

    create diff object
    Running the compareObjects function will return a object with
    differences. It will show which keys are deleted and which values of the
    keys have changed.

--- jsondiff.js
+++ jsondiff.js
@@ -6,28 +6,37 @@ function printAllKeys(object) {
         if (typeof object[key] === "object") {
             printAllKeys(object[key]);
         } else {
-            console.log(key);
+            console.log(key + object[key]);
         }
     }
 }
 
 
+function compareObjects(obj1, obj2) {
+    var diffs = {};
+    for (let key in obj1) {
+        compare(obj1[key], obj2[key], key, diffs);
+    }
+    return diffs;
+}
 
-function compareObjects(obj1, obj2, diff){
-    for(key in obj1){
-        if(obj2.hasOwnProperty(key)){
-            if(typeof obj1[key] === "object"){
-                compareObjects(obj1[key], obj2[key]);
-            }else{
-                console.log("both objects have the " + key + " key");
-                if(obj1[key] !== obj2[key]){
-                    console.log("but the content has changed");
-                }
-            }
-        }else{
-            console.log("second object is missing the " + key + "key");
+function compare(item1, item2, key, diffs) {
+    if(item2 === undefined){
+        diffs[key] = undefined;
+        return;
+    }
+    if (typeof item1 === "object") {
+        const objDiff = compareObjects(item1, item2);
+        if(Object.keys(objDiff).length > 0){
+            diffs[key] = objDiff;
+        }
+        return;
+    } else {
+        if (item1 !== item2) {
+            diffs[key] = item2;
         }
     }
 }
 
-compareObjects(object1, object2, {});
+const d = compareObjects(object1, object2, {});
+printAllKeys(d);
